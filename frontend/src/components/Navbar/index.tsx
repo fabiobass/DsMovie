e17322e14d9ac170151f753683ce46e1eff6ccb9
Fabio Silva <fsilva.developer@gmail.com>
2022-05-09 23:25:05 -0300

    Navbar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,15 +1,16 @@
 import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
+import './styles.css';
 
 function Navbar() {
 
     return (<header>
-        <nav>
-            <div>
+        <nav className="container">
+            <div className="dsmovie-nav-content">
                 <h1>DSMovie</h1>
-                <a href="https://github.com/fabiobass">
-                    <div>
+                <a href="https://github.com/fabiobass" target="_blank" rel="noreferrer">
+                    <div className="dsmovie-contact-container">
                         <GithubIcon />
-                        <p>/devsuperior</p>
+                        <p className="dsmovie-contact-link">/devsuperior</p>
                     </div>
                 </a>
             </div>
